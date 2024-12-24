3759bf26f7e46e5308a53a0117e05b32d97a094f
Michael Kamyshov <medmk@yandex.ru>
2024-12-24 18:56:26 +0100

    [001] Interactable only top slots

--- Scripts/card_slot.gd
+++ Scripts/card_slot.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
 var card_in_slot = false
+var slot_empty = true
+var slot_available_for_drop = false
+var slot_interactable = false
+var BG_Alpha_notInteractable: float = 0.3
+var BG_Alpha_Interactable: float = 0.7
 
-func show_state(array_pos, array_number):
+func show_slot_state(array_pos, array_number, _slot_empty, _slot_available_for_drop, _slot_interactable):
 	$Array_Pos.text = array_pos
 	$Array_Number.text = array_number
+	if slot_empty:
+		$BG/ColorRect.visible = false
+	else:
+		$BG/ColorRect.visible = true
+		
+	if _slot_interactable:
+		$BG.modulate = Color(1,1,1,BG_Alpha_Interactable)
+	else:
+		$BG.modulate = Color(1,1,1,BG_Alpha_notInteractable)
+		
+	slot_interactable = _slot_interactable
+	
 	
 func show_card_info(name):
 	$Card_Info.text = name
+
 	
--- Scripts/grid_manager.gd
+++ Scripts/grid_manager.gd
@@ -6,6 +6,18 @@ const CARD_SLOT_PATH = "res://Scenes/card_slot.tscn"
 @export var hight_offset: int = 200
 var first_slot_pos_x
 var first_slot_pos_y
+var slot_empty = true
+var slot_available_for_drop = false
+var slot_interactable = false
+var slot_earth_points = 0
+var slot_air_points = 0
+var slot_water_points = 0
+var slot_fire_points = 0
+var slot_enemy_type = 0 #0 - land 1 - enemy 2 - bonus
+var slot_player = false
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	first_slot_pos_x = get_viewport().size.x/2 - width_offset * columns/2 + width_offset/2
@@ -14,13 +26,19 @@ func _ready() -> void:
 	var array_number = 0
 	for y in range(rows):
 		for x in range(columns):
+			#top slots interactable
+			if y < 1 and x <= 3:
+				slot_interactable = true
+			else:
+				slot_interactable = false
+				
+				
 			var new_slot = card_slot.instantiate()
 			add_child(new_slot)
 			new_slot.position = Vector2(first_slot_pos_x + width_offset*x, first_slot_pos_y + hight_offset*y)
 			var array_pos = str(x) + " / " + str(y)
-			new_slot.show_state(array_pos, str(array_number))
+			new_slot.show_slot_state(array_pos, str(array_number), slot_empty, slot_available_for_drop, slot_interactable)
 			array_number+=1
-			print(new_slot.position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
